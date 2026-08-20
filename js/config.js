44431bcb1d5011c0741915da4d6471faa3ebc982
monkidkhtan-code <monkid.khtan@gmail.com>
2026-08-20 21:35:47 +0800

    update

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
  * KH AGRIFARM - CONFIGURATION
  */
 const APP_CONFIG = {
-  version: "v10.81",
+  version: "v10.82",
   buildDate: "2026.08.20",
   farmName: "KH Agrifarm",
   farmAddress: "LOT 20371, Jalan Sgg 6/3, Kampung Sungai Gulang Gulang, 45500 Tanjong Karang, Selangor",
--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /**
  * KH AGRIFARM - SERVICE WORKER (NETWORK FIRST STRATEGY)
  */
-const CACHE_NAME = 'kh-agrifarm-v10.81';
+const CACHE_NAME = 'kh-agrifarm-v10.82';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
