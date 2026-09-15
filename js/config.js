83edddd0e680bb2f2274148a0c294ab339757b67
monkidkhtan-code <monkid.khtan@gmail.com>
2026-09-15 09:23:38 +0800

    Added cocopeat buffering

--- js/config.js
+++ js/config.js
@@ -2,8 +2,8 @@
  * KH AGRIFARM - CONFIGURATION
  */
 const APP_CONFIG = {
-  version: "v11.53",
-  buildDate: "2026.09.14",
+  version: "v11.54",
+  buildDate: "2026.09.15",
   farmName: "KH Agrifarm",
   farmAddress: "LOT 20371, Jalan Sgg 6/3, Kampung Sungai Gulang Gulang, 45500 Tanjong Karang, Selangor",
   season: "Season 6 (June 2026 - Jan 2027)",
